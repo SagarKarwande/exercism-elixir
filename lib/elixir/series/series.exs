5daf4a9dc52d8fe36013414271e0a608ca7df3e9
Sagar Karwande <sagar_karwande123@yahoo.com>
2017-10-26 18:35:57 +0530

    string series solution

---
+++ lib/elixir/series/series.exs
@@ -0,0 +1,15 @@
+defmodule StringSeries do
+  @doc """
+  Given a string `s` and a positive integer `size`, return all substrings
+  of that size. If `size` is greater than the length of `s`, or less than 1,
+  return an empty list.
+  """
+  @spec slices(s :: String.t(), size :: integer) :: list(String.t())
+  def slices(_, size) when size < 1, do: []
+  def slices(s, size) do
+    s
+    |>String.split("", trim: true)
+    |> Enum.chunk(size, 1)
+    |> Enum.map(&Enum.join/1)
+  end
+end
